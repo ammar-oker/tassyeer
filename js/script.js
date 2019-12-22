6d9c13bcbbfbb233c4713fe92583f62373934975
ammar-oker <ammarokar12345@gmail.com>
2019-12-22 18:47:36 +0300

    Upgrade the whole website

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict";
 
   // Smooth scroll
-  $("#nav .main-nav a[href^='#']").on("click", function(e) {
+  $("#nav .main-nav a[href^='#']").on("click", function (e) {
     e.preventDefault();
     var hash = this.hash;
     $("html, body").animate(
@@ -13,7 +13,7 @@
     );
   });
 
-  $("#back-to-top").on("click", function() {
+  $("#back-to-top").on("click", function () {
     $("body,html").animate(
       {
         scrollTop: 0
@@ -24,27 +24,43 @@
 
   ///////////////////////////
   // Btn nav collapse
-  $("#nav .nav-collapse").on("click", function() {
-    $("#nav").toggleClass("open");
+
+  $('#open-sidebar').click(function (e) {
+    e.stopPropagation();
+    $('#mySidenav').addClass('show-menu');
   });
+  $('#close-sidebar').click(function (e) {
+    e.stopPropagation();
+    $('#mySidenav').removeClass('show-menu');
+  });
+  $('#mySidenav').click(function (e) {
+    if(e.target.id != 'menu-btn' && e.target.id != 'close-sidebar') {
+      e.stopPropagation();
+    }
+  });
+  $('body,html').click(function (e) {
+    if(e.target.id != 'menu-btn' && e.target.id != 'close-sidebar') {
+      $('#mySidenav').removeClass('show-menu');
+    }
+  });
+
+
 
   ///////////////////////////
   // On Scroll
-  $(window).on("scroll", function() {
+  $(window).on("scroll", function () {
     var wScroll = $(this).scrollTop();
 
     // Fixed nav
-    wScroll > 1
+    wScroll > 100
       ? $("#nav").addClass("fixed-nav")
       : $("#nav").removeClass("fixed-nav");
 
-    // Back To Top Appear
-    wScroll > 700 ? $("#back-to-top").fadeIn() : $("#back-to-top").fadeOut();
   });
 
   ///////////////////////////
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     //"how to use" carousel for mobiles only
     var phonesSwiper = new Swiper(".phones-swiper", {
       slidesPerView: 1,
@@ -52,7 +68,7 @@
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
-        renderBullet: function(index, className) {
+        renderBullet: function (index, className) {
           return (
             '<button class="' + className + '">' + (index + 1) + "</button>"
           );
@@ -64,45 +80,82 @@
         }
       }
     });
-  });
 
-  var postsSwiper = new Swiper(".posts-swiper", {
-    loop: true,
-    slidesPerView: 1,
-    grabCursor: true,
-    spaceBetween: 20,
-    slidesOffsetBefore: 10,
-    slidesOffsetAfter: 20,
-	width: 280,
-	autoplay: {
-		delay: 7000,
-	  },
-    pagination: {
-      el: ".swiper-pagination1",
-      clickable: true
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        width: 640
+    var postsSwiper = new Swiper(".posts-swiper", {
+      loop: true,
+      slidesPerView: 1,
+      grabCursor: true,
+      spaceBetween: 20,
+      slidesOffsetBefore: 10,
+      slidesOffsetAfter: 20,
+      width: 280,
+      autoplay: {
+        delay: 7000,
       },
-      1024: {
-        slidesPerView: 3,
-        width: 1024,
-        slidesOffsetBefore: 0,
-        slidesOffsetAfter: 0
+      pagination: {
+        el: ".swiper-pagination1",
+        clickable: true
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          width: 640
+        },
+        1024: {
+          slidesPerView: 3,
+          width: 1024,
+          slidesOffsetBefore: 0,
+          slidesOffsetAfter: 0
+        }
       }
-    }
+    });
+    var servicesSwiper = new Swiper(".services-swiper", {
+      loop: true,
+      slidesPerView: 1,
+      grabCursor: true,
+      spaceBetween: 20,
+      slidesOffsetBefore: 10,
+      slidesOffsetAfter: 20,
+      width: 280,
+      autoplay: {
+        delay: 7000,
+      },
+      pagination: {
+        el: ".swiper-pagination1",
+        clickable: true
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          width: 640
+        },
+        1024: {
+          slidesPerView: 3,
+          width: 1024,
+          slidesOffsetBefore: 0,
+          slidesOffsetAfter: 0
+        }
+      }
+    });
+  
+  
+    var iphoneXSwiper = new Swiper('.iphone-x-swiper', {
+      // Optional parameters
+      loop: true,
+      autoplay: {
+        delay: 3000,
+      }
+    })
+  
   });
-
-
-  var iphoneXSwiper = new Swiper ('.iphone-x-swiper', {
-    // Optional parameters
-	loop: true,
-	autoplay: {
-		delay: 3000,
-	  }
-  })
 
 
 })(jQuery);
+
+//mobile phone helper
+
+var cleave = new Cleave('.mobile', {
+  delimiter: ' ',
+  blocks: [1, 3, 3, 4],
+  uppercase: true
+});
